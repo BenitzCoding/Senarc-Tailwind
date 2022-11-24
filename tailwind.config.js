309498a9b8216c037dc78cd607525dbfb9ceccb7
BenitzCoding <benitzcoding@yahoo.com>
2022-11-24 20:30:27 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js,vue}"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: "1rem"
+    }
+  },
+  plugins: []
+};
